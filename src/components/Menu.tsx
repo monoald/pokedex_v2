b0063d77710b0f097a1e531366cd3d75bea7781a
Carlos S Aldazosa <carlos.aldazosa@outlook.com>
2022-12-05 18:56:38 -0400

    [FEAT] Pokemon card list

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -4,9 +4,18 @@ import CardMenuList from '../containers/CardMenuList';
 
 import '../styles/Menu.scss';
 
-const Menu = () => {
+interface Props {
+  menuToggle: () => void;
+}
+
+const Menu = ({ menuToggle }: Props) => {
   return (
     <section className='Menu'>
+      <button
+        className='Menu__close-button'
+        type='button'
+        onClick={menuToggle}
+      ></button>
       <CardMenuList />
     </section>
   );
---
+++ src/containers/PokemonCardList.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import '../styles/PokemonCardList.scss';
+
+interface Props {
+  children: JSX.Element;
+}
+
+const PokemonCardList = ({ children }: Props) => {
+  return <ul className='Pokemon-Card-List'>{children}</ul>;
+};
+
+export default PokemonCardList;
